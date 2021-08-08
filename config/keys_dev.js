389d5c89a45d4a7fafaa1e971cb9b0ef19b60e8a
Patrick Sihn <phsihn@gmail.com>
2021-08-08 00:40:37 -0400

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	mongoURI:
-		'mongodb+srv://dbUser:upK0QVS5TFZAsgFD@job-journey0.z2tjt.mongodb.net/job-journey0?retryWrites=true&w=majority',
-	secretOrKey: '[9A8evY6DY$sJ$b_',
-};
