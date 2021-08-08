0427456751d6fadca357dcee0f83098aa828b3d5
phsihn <phsihn@gmail.com>
2021-08-08 00:01:07 -0400

    initial commit for heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const app = express();
 const passport = require('passport');
+const path = require('path');
 
 // routes
 const users = require('./routes/api/users');
@@ -23,6 +24,13 @@ app.use(express.json());
 
 app.use('/api/users', users);
 app.use('/api/jobs', jobs);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('frontend/build'));
+	app.get('/', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
